a213154a30d156a0303c12eea80b0df656a86000
kirillcisco <asasinok01@gmail.com>
2022-11-05 20:45:18 +0300

    Added "hypergate" as monitor to sniff packet

--- Models/Packet.cs
+++ Models/Packet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,27 +23,84 @@ namespace TarTarSniffer.Models
             IPv6 = 41,
             IPx = 1000,
             ND = 77,
+            Raw = 255,
+            Spx = 1256,
             PUP = 12,
             TCP = 6,
             UDP = 17,
-            OTHERS = -1
+            UNKNOWN = -1,
+            UNCPEIFIED = 0
         }
 
         // the original packet sniffed in the underlying layer
         private byte[] packet_Raw;
 
         // the sniffed time;
-        private DateTime dateTime;
+        private DateTime sniffedTime;
 
         // packet variables
         private Packet_Protocol packet_Protocol;
-        private IPAddress packet_SRC_ip;
-        private IPAddress packet_DEST_ip;
+        private IPAddress packet_SRC_IP;
+        private IPAddress packet_DEST_IP;
         private IPAddress packet_TTL;
 
         private int packet_SRC_PORT;
         private int packet_DEST_PORT;
         private int packet_totalLength;
         private int packet_headLength;
+
+        public Packet(byte[] raw)
+        {
+            // all the following exceptions should be caught when invoking this constructor;
+            if (raw == null)
+                throw new ArgumentNullException();
+
+            // when the orginal length is less than 20, it must be wrong;
+            if (raw.Length < 20)
+                throw new ArgumentException();
+
+            packet_Raw = raw;
+
+            // sniff time
+            sniffedTime = DateTime.Now;
+
+            // get the headlength in the packet;
+            packet_headLength = (packet_Raw[0] & 0x0F) * 4;
+
+            if ((raw[0] & 0x0F) < 5)
+                throw new ArgumentException(); // header is wrong for the length is incorrect;
+
+            // get packet type
+            if (Enum.IsDefined(typeof(Packet_Protocol), (int)packet_Raw[9]))
+                packet_Protocol = (Packet_Protocol)packet_Raw[9];
+            else
+                packet_Protocol = Packet_Protocol.UNCPEIFIED;
+
+            packet_SRC_IP = new IPAddress(BitConverter.ToUInt32(packet_Raw, 12));
+            packet_DEST_IP = new IPAddress(BitConverter.ToUInt32(packet_Raw, 16));
+            packet_totalLength = packet_Raw[2] * 256 + packet_Raw[3];
+
+            // handle TCP OR UDP
+            if (packet_Protocol == Packet_Protocol.TCP || packet_Protocol == Packet_Protocol.UDP)
+            {
+                packet_SRC_PORT = packet_Raw[packet_headLength] * 256 + packet_Raw[packet_headLength + 1];
+                packet_DEST_PORT = packet_Raw[packet_headLength + 2] * 256 + packet_Raw[packet_headLength + 3];
+
+                if (packet_Protocol == Packet_Protocol.TCP)
+                {
+                    packet_headLength += 20;
+                }
+                else if (packet_Protocol == Packet_Protocol.UDP)
+                {
+                    packet_headLength += 8;
+                }
+            }
+            else
+            {
+                packet_SRC_PORT = -404;
+                packet_DEST_PORT = -404;
+            }
+
+        }
     }
 }
---
+++ Sniffer/Hypergate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TarTarSniffer.Sniffer
+{
+    class Hypergate
+    {
+
+        private const int BUFFER_SIZE = 1024 * 1024;
+        private const int IOC_VENDOR = 0x18000000;
+        private const int IOC_IN = -2147483648; //0x80000000;
+        private const int SIO_RCVALL = IOC_IN | IOC_VENDOR | 1;
+
+        private Socket hypergate_Socket;
+        private IPAddress _ipAddress;
+        private byte[] hypergate_buffer;
+
+        // https://learn.microsoft.com/ru-ru/dotnet/api/system.net.sockets.socketflags?view=net-6.0
+        enum Socket_Flags
+        {
+            Broadcast = 1024,
+            ControlDataTruncated = 512,
+            DontRoute = 4,
+            Multicast = 2048,
+            OutOfBand = 1,
+            Partial = 32768,
+            Peek = 2,
+            Truncated = 256,
+            None = 0
+        }
+        private Socket_Flags socket_Flags;
+
+        public Hypergate(IPAddress ip)
+        {
+            this._ipAddress = ip;
+            this.hypergate_buffer = new byte[BUFFER_SIZE];
+        }
+        
+        public void StartMonitor()
+        {
+            if(hypergate_Socket == null)
+            {
+                try
+                {   
+                    // ipv4 or ipv6
+                    switch (_ipAddress.AddressFamily)
+                    {
+                        case AddressFamily.InterNetwork:
+                            hypergate_Socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, System.Net.Sockets.ProtocolType.IP);
+                            break;
+                        case AddressFamily.InterNetworkV6:
+                            hypergate_Socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Raw, System.Net.Sockets.ProtocolType.IP);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    hypergate_Socket.Bind(new IPEndPoint(_ipAddress, 0));
+
+                    // https://learn.microsoft.com/ru-ru/dotnet/api/system.net.sockets.socket.iocontrol?view=net-6.0
+                    hypergate_Socket.IOControl(SIO_RCVALL, BitConverter.GetBytes((int)1), null);
+
+                    hypergate_Socket.BeginReceive(hypergate_buffer, 0, hypergate_buffer.Length, SocketFlags.None, new AsyncCallback(this.OnReceive), null);;
+                }
+                catch (Exception e)
+                {
+                    hypergate_Socket.Close();
+                    hypergate_Socket = null;
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        public void StopMonitor()
+        {
+            hypergate_Socket.Close();
+            hypergate_Socket = null;
+        }
+
+        private void OnReceive(IAsyncResult recieved_raw)
+        {
+            try
+            {
+                int len = hypergate_Socket.EndReceive(recieved_raw);
+                if (hypergate_Socket != null)
+                {
+                    byte[] receivedBuffer = new byte[len];
+                    Array.Copy(hypergate_buffer, 0, receivedBuffer, 0, len);
+                    try
+                    {
+                        Models.Packet packet = new Models.Packet(receivedBuffer);
+                        OnNewPacket(packet);
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        Console.WriteLine(ane.ToString());
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.ToString());
+                    }
+                }
+
+                hypergate_Socket.BeginReceive(hypergate_buffer, 0, hypergate_buffer.Length, SocketFlags.None, new AsyncCallback(this.OnReceive), null);
+            }
+            catch
+            {
+                StopMonitor();
+            }
+        }
+
+
+        protected void OnNewPacket(Models.Packet p)
+        {
+            if (newPacketEventHandler != null)
+            {
+                newPacketEventHandler(this, p);
+            }
+        }
+
+        public event NewPacketEventHandler newPacketEventHandler;
+        public delegate void NewPacketEventHandler(Hypergate hypergate, Models.Packet p);
+    }
+}
